f6e33b7b42c5091416b39d358668f73ec17c9a4b
Choi Tae gyu <suker800@gmail.com>
2022-12-22 21:03:05 +0900

    패턴 5 . 프로토타입 패턴 구현

---
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_05_prototype/_02_after/GithubIssue.java
@@ -0,0 +1,68 @@
+package me.whiteship.designpatterns._01_creational_patterns._05_prototype._02_after;
+
+import java.util.Objects;
+
+public class GithubIssue implements Cloneable{
+
+    private int id;
+
+    private String title;
+
+    private GithubRepository repository;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GithubIssue that = (GithubIssue) o;
+        return id == that.id && Objects.equals(title, that.title) && Objects.equals(repository, that.repository);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, repository);
+    }
+
+    public GithubIssue(GithubRepository repository) {
+        this.repository = repository;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public GithubRepository getRepository() {
+        return repository;
+    }
+
+    public String getUrl() {
+        return String.format("https://github.com/%s/%s/issues/%d",
+                repository.getUser(),
+                repository.getName(),
+                this.getId());
+    }
+
+    @Override
+    public GithubIssue clone() {
+        GithubRepository githubRepository = new GithubRepository();
+        githubRepository.setName(this.repository.getName());
+        githubRepository.setUser(this.repository.getUser());
+        GithubIssue githubIssue = new GithubIssue(githubRepository);
+        githubIssue.setId(this.getId());
+
+        githubIssue.setTitle(this.getTitle());
+        return githubIssue;
+    }
+}
